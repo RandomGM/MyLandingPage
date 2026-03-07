b8953394f4969940d92f06dbafaabc542ae919aa
RandomGM <nii.machado111@gmail.com>
2026-03-07 09:00:31 -0300

    Aula 4
    JavaScript

---
+++ Aula-1/JavaScript.js
@@ -0,0 +1,40 @@
+
+function alerta123(){
+    alert("Erro")
+
+}
+
+function alerta321(){
+    alert("@*$$#$*@&$*")
+
+}
+
+
+//document.addEventListener('click',alerta123)
+
+//document.addEventListener('dblclick', alerta321)
+
+//document.addEventListener('DOMContentLoaded')
+
+
+let projetos = [
+
+    {titulo: "Projeto 1", descricao: "Meu Projeto 1", link: "#"},
+    {titulo: "Projeto 2", descricao: "Meu Projeto 2", link: "#"},
+    {titulo: "Projeto 3", descricao: "Meu projeto 3", link: "#"}
+
+]
+
+console.log (projetos)
+
+projetos.forEach(function (projeto) {
+
+console.log(projeto)
+
+//injetar o html
+
+})
+
+
+
+
