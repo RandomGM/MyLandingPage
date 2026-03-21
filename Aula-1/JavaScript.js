6d6e5612143d28b2b6ccaff6669d754404767a94
RandomGM <nii.machado111@gmail.com>
2026-03-21 09:03:33 -0300

    21/03
    project-cards, (meus projetos) com o p5js

--- Aula-1/JavaScript.js
+++ Aula-1/JavaScript.js
@@ -19,21 +19,29 @@ function alerta321(){
 
 let projetos = [
 
-    {titulo: "Projeto 1", descricao: "Meu Projeto 1", link: "#"},
+    {titulo: "Projeto 1", descricao: "Laço de repetição", link: "https://editor.p5js.org/Nicolasmp12321/full/f6TvSHfUA"},
+
     {titulo: "Projeto 2", descricao: "Meu Projeto 2", link: "#"},
+
     {titulo: "Projeto 3", descricao: "Meu projeto 3", link: "#"}
 
 ]
 
-console.log (projetos)
+var projectgrid = document.querySelector(".project-grid")
 
-projetos.forEach(function (projeto) {
 
-console.log(projeto)
+    projetos.forEach(function (projeto) {
 
-//injetar o html
+        var card = document.createElement("a"); // <a> <a/>
+        card.className = "project-card"; // <a class= "project-card"> <a/>
+        card.href = projeto.link; // <a class="projet-card" href = "link do projeto"> <a/>
+        card.innerHTML = "h3" + projeto.titulo + "<h3> <p>" + projeto.descricao;
+        projectgrid.appendChild(card);
 
-})
+
+});
+
+
 
 
 
